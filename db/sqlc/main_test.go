385fd1afc4397e3c9f4e1d41b508230cf68d7207
Luis Teixeira <teixeira13@gmail.com>
2024-01-31 21:22:12 +0100

    Add get account api tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,21 +7,21 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/luisteixeira/simplebank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadCofig("../..")
 	if err != nil {
-		log.Fatal("Cannot connect to db:", err)
+		log.Fatal("Cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatalf("Cannot connect to db: %s - config: %s", err, config)
 	}
 
 	testQueries = New(testDB)
